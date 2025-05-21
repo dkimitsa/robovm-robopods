a5b86108f4475f68bb0fa86ebd54c1e2d298a575
dkimitsa <demyan.kimitsa@gmail.com>
2025-05-21 21:12:14 +0300

    * Facebook Audience Network updated: v6.16.0 -> v6.17.1 (no api changes)

--- facebook/ios-audience/src/main/java/org/robovm/pods/facebook/audience/FBAudienceNetworkAds.java
+++ facebook/ios-audience/src/main/java/org/robovm/pods/facebook/audience/FBAudienceNetworkAds.java
@@ -48,7 +48,7 @@ import org.robovm.apple.avfoundation.*;
     /*<ptr>*/public static class FBAudienceNetworkAdsPtr extends Ptr<FBAudienceNetworkAds, FBAudienceNetworkAdsPtr> {}/*</ptr>*/
     /*<bind>*/static { ObjCRuntime.bind(FBAudienceNetworkAds.class); }/*</bind>*/
     /*<constants>*/
-    public static final String getVersion = "6.16.0";
+    public static final String getVersion = "6.17.1";
     /*</constants>*/
     /*<constructors>*/
     public FBAudienceNetworkAds() {}
